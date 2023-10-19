5e96d4bb3a56e3a3de6f74bed09c69aa8805997e
Rajesh Lakkakula <rajeshlakkakula536@gmail.com>
2023-10-19 12:28:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# firebase_meetup_app
+
+Firebase Meetup App
+
+## Getting Started
+
+ To Build an Event and Guestchat App supports Android,ios,Web.
+
---
+++ android/app/src/main/kotlin/com/example/firebase_meetup_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_meetup_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
