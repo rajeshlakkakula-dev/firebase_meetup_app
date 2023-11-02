6f87dcea46db2c8505cb4b9d46e7775b15e69900
ChakaliMadhu6302769895 <Gauthamroyalz12324@gmail.com>
2023-11-02 11:40:50 +0530

    initial commit

--- lib/SecondScreen.dart
+++ lib/SecondScreen.dart
@@ -1,71 +1,66 @@
-
-import 'dart:ui';
+import 'package:branch_madhu_c/main.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:branch_madhu_c/SecondScreen.dart';
 
 void main(){
-  runApp(
-      MaterialApp(
-          home:MyApp()));
+  runApp(SecondScreen());
 }
 
-class MyApp extends StatelessWidget{
+class SecondScreen extends StatelessWidget {
+
+  final formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:SingleChildScrollView(
-        child:MyCustomForm(),
+      body: SingleChildScrollView(
+          child: Myhome()
       ),
     );
   }
 }
-class MyCustomForm extends StatefulWidget {
 
+class Myhome extends StatefulWidget{
   @override
-  State<MyCustomForm> createState() => MyCustomFormState();
+  State<Myhome> createState() => _MyHomeState();
+
 }
 
-class MyCustomFormState extends State<MyCustomForm> {
-  @override
+class _MyHomeState  extends State<Myhome>{
 
-  final _formkey = GlobalKey <FormState> ();
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-
+  final _formkey = GlobalKey<FormState>();
 
   get buttonenabled => true;
+
+  @override
   Widget build(BuildContext context) {
     return Form(
       key: _formkey,
-      child:Column(
+      child: Column(
         children: [
-          Padding(padding: EdgeInsets.fromLTRB(0, 220, 270 , 10),
-            child: Text('Register' , style: TextStyle(fontSize: 30),),
+          Padding(padding: EdgeInsets.fromLTRB(0, 220, 285, 20),
+              child: Text('Sign in' , style: TextStyle(fontSize: 30),)
           ),
 
           Padding(padding: EdgeInsets.symmetric(horizontal: 18),
             child:Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text("Already have an account?"),
+                Text("Don't have an account?"),
                 TextButton(
-                  onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
-                  },
-                  child: Text("Sign in"),
+                  onPressed: (){},
+                  child: Text("Register"),
                 ),
               ],
             ),
           ),
 
-
           Padding(padding: EdgeInsets.all(20),
             child: TextFormField(
-              controller: emailController,
               decoration: InputDecoration(
                   labelText: ("Email")
               ),
+
               validator: (value){
                 if(value==null ||value.isEmpty){
                   return "Enter Email";
@@ -83,38 +78,28 @@ class MyCustomFormState extends State<MyCustomForm> {
           ),
           Padding(padding: EdgeInsets.all(20),
             child: TextFormField(
-              controller: passwordController,
               decoration: InputDecoration(
                   labelText: ("Password")
               ),
-
               validator: (value){
-                if(value!.isEmpty) {
+                if(value==null ||value.isEmpty){
                   return "Enter Password";
                 }
-                else if(passwordController.text.length<8){
-                  return "Atleast 8 characters";
-                }
+                return null;
               },
             ),
           ),
-          Padding(padding: EdgeInsets.all(20),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: ("Confirm password")
-              ),
 
-              validator: (value){
-                if(value!.isEmpty) {
-                  return "Password Should Not Match";
-                }
-                else if(passwordController.text.length<8){
-                  return "Atleast 8 characters";
-                }
-              },
 
+
+          Padding(padding: EdgeInsets.fromLTRB(250, 10, 0, 10),
+            child: TextButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) =>SecondScreen()));
+            },
+              child: Text("Forgot password?"),
             ),
           ),
+
           Container(
             padding: EdgeInsets.all(32),
             child: OutlinedButton(
@@ -124,17 +109,17 @@ class MyCustomFormState extends State<MyCustomForm> {
               onPressed: buttonenabled?(){
 
                 if (_formkey.currentState!.validate()) {
-                  emailController.clear();
-                  passwordController.clear();
+
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(content: Text('REGISTRATION SUCCESSFUL..')),
                   );
                 }
 
               } :null,
-              child: Text("Register"),
+              child: Text("Sign in"),
             ),
           ),
+
         ],
       ),
     );
